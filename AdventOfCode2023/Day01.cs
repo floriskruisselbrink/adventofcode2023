c87a95d59cb251eebc8851e0c4db1be1ef2394a6
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2023-11-27 12:37:07 +0100

    Add project files.

---
+++ AdventOfCode2023/Day01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    public class Day01 : BaseDay
+    {
+        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+        public override ValueTask<string> Solve_2() => new("part2");
+    }
+}
