b2931fd4d27c8d65273897fbd57bc6c9d90bc2ad
Floris Kruisselbrink <floris@vloris.nl>
2023-12-01 15:03:02 +0100

    Optimalisatie en tests

---
+++ AdventOfCode2023.Test/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Test;
+
+public static class SolutionTests
+{
+    [TestCase(typeof(Day01), "54597", "54504")]
+    public static void Test(Type type, string solution1, string solution2)
+    {
+        if (Activator.CreateInstance(type) is BaseProblem instance)
+        {
+            Assert.Multiple(async () =>
+            {
+                Assert.That(await instance.Solve_1(), Is.EqualTo(solution1));
+                Assert.That(await instance.Solve_2(), Is.EqualTo(solution2));
+            });
+        }
+        else
+        {
+            Assert.Fail($"{type} is not a BaseProblem");
+        }
+    }
+}
--- AdventOfCode2023/Day01.cs
+++ AdventOfCode2023/Day01.cs
@@ -4,53 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventOfCode2023
+namespace AdventOfCode2023;
+
+public class Day01 : BaseDay
 {
-    public class Day01 : BaseDay
+    private readonly string[] _input;
+
+    public Day01()
     {
-        private readonly string[] _input;
-
-        public Day01()
-        {
-            _input = File.ReadAllLines(InputFilePath);
-        }
-
-        public override ValueTask<string> Solve_1()
-        {
-            var sum = _input.Select(line => int.Parse(FindFirstDigit(line).ToString() + FindLastDigit(line).ToString()))
-                            .Sum();
-            return new(sum.ToString());
-        }
-
-        public override ValueTask<string> Solve_2()
-        {
-            var sum = _input.Select(ReplaceNumbers)
-                            .Select(line => int.Parse(FindFirstDigit(line).ToString() + FindLastDigit(line).ToString()))
-                            .Sum();
-            return new(sum.ToString());
-        }
-
-        private string ReplaceNumbers(string input)
-        {
-            return input.Replace("one", "o1e")
-                        .Replace("two", "t2o")
-                        .Replace("three", "t3e")
-                        .Replace("four", "f4r")
-                        .Replace("five", "f5e")
-                        .Replace("six", "s6x")
-                        .Replace("seven", "s7n")
-                        .Replace("eight", "e8t")
-                        .Replace("nine", "n9e");
-        }
-
-        private char FindFirstDigit(IEnumerable<char> input)
-        {
-            return input.SkipWhile(c => !char.IsDigit(c)).First();
-        }
-
-        private char FindLastDigit(IEnumerable<char> input)
-        {
-            return FindFirstDigit(input.Reverse());
-        }
+        _input = File.ReadAllLines(InputFilePath);
     }
+
+    public override ValueTask<string> Solve_1() => new(
+        _input.Select(CalibrationValue)
+              .Sum()
+              .ToString()
+        );
+
+    public override ValueTask<string> Solve_2() => new(
+        _input.Select(ReplaceNumbers)
+              .Select(CalibrationValue)
+              .Sum()
+              .ToString()
+        );
+
+    private string ReplaceNumbers(string input)
+    {
+        return input.Replace("one", "o1e")
+                    .Replace("two", "t2o")
+                    .Replace("three", "t3e")
+                    .Replace("four", "f4r")
+                    .Replace("five", "f5e")
+                    .Replace("six", "s6x")
+                    .Replace("seven", "s7n")
+                    .Replace("eight", "e8t")
+                    .Replace("nine", "n9e");
+    }
+
+    private int CalibrationValue(string input) => int.Parse(input.FindFirstDigit() + input.FindLastDigit());
+}
+
+internal static class Day01Extensions
+{
+    internal static string FindFirstDigit(this string input) => input.SkipWhile(c => !char.IsDigit(c)).First().ToString();
+    internal static string FindLastDigit(this string input) => input.Reverse().SkipWhile(c => !char.IsDigit(c)).First().ToString();
 }
