f24f4525cc9c2d9ecaed38a09bb3ad6c658ca83c
Floris Kruisselbrink <floris@vloris.nl>
2023-12-04 19:15:37 +0100

    AoC 2023 day 3

---
+++ AdventOfCode2023/Day03.cs
@@ -0,0 +1,98 @@
+﻿namespace AdventOfCode2023;
+
+public class Day03 : BaseDay
+{
+    private readonly Grid<char> _input;
+
+    public Day03()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1() => new(
+            FindPartNumbers(_ => true)
+                .Select(p => p.number)
+                .Sum()
+                .ToString()
+            );
+
+    public override ValueTask<string> Solve_2() => new(
+            FindPartNumbers(c => c == '*')
+                .GroupBy(part => (part.x, part.y))
+                .Where(group => group.Count() == 2)
+                .Select(group => group.First().number * group.Last().number)  
+                .Sum()
+                .ToString()
+            );
+
+    private IEnumerable<(int x, int y, int number)> FindPartNumbers(Func<char, bool> symbolTest)
+    {
+        var bounds = _input.CalculateBounds();
+
+        for (int y = bounds.minY; y <= bounds.maxY; y++)
+        {
+            int currentNumber = 0;
+            bool currentAdjacent = false;
+            int currentX = -1;
+            int currentY = -1;
+            for (int x = bounds.minX; x <= bounds.maxX; x++)
+            {
+                if (!_input[x, y].Exists)
+                {
+                    if (currentAdjacent && currentNumber > 0)
+                    {
+                        yield return (currentX, currentY, currentNumber);
+                    }
+                    currentNumber = 0;
+                    currentAdjacent = false;
+                }
+                else if (char.IsDigit(_input[x, y]))
+                {
+                    currentNumber = currentNumber * 10 + (_input[x, y] - '0');
+                    foreach (var neighbour in _input.Neighbours((x, y), true))
+                    {
+                        if (!char.IsDigit(neighbour.Value) && symbolTest(neighbour.Value))
+                        {
+                            currentAdjacent = true;
+                            currentX = neighbour.X;
+                            currentY = neighbour.Y;
+                        }
+                    }
+                }
+                else if (currentAdjacent && currentNumber > 0)
+                {
+                    yield return (currentX, currentY, currentNumber);
+                    currentNumber = 0;
+                    currentAdjacent = false;
+                }
+            }
+
+            if (currentAdjacent && currentNumber > 0)
+            {
+                yield return (currentX, currentY, currentNumber);
+            }
+        }
+    }
+    private Grid<char> ParseInput()
+    {
+        var lines = File.ReadAllLines(InputFileDirPath + "\\03.txt");
+        var grid = new Grid<char>();
+
+        int y = 0;
+        foreach (var line in lines)
+        {
+            int x = 0;
+            foreach (char ch in line)
+            {
+                if (ch != '.')
+                {
+                    grid[x, y] = ch;
+                }
+                x++;
+            }
+            y++;
+        }
+
+        return grid;
+    }
+}
---
+++ AdventOfCode2023/Grid.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace AdventOfCode2023;
+
+public class Grid<T>
+{
+    private readonly IDictionary<(int, int), T> _grid = new Dictionary<(int, int), T>();
+
+    public GridMember<T> this[int x, int y]
+    {
+        get { return _grid.TryGetValue((x, y), out var t) ? new GridMember<T>(x, y, t) : new GridMember<T>(x, y); }
+        set { _grid[(x, y)] = value.Value; }
+    }
+
+    public IEnumerable<(int x, int y, T value)> AllMembers()
+    {
+        foreach (var t in _grid)
+        {
+            yield return (t.Key.Item1, t.Key.Item2, t.Value);
+        }
+    }
+
+    public IEnumerable<GridMember<T>> Neighbours((int x, int y) location, bool includeDiagonal)
+    {
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0 || (x * y != 0 && !includeDiagonal))
+                {
+                    continue;
+                }
+
+                var m = this[location.x + x, location.y + y];
+                if (m.Exists)
+                {
+                    yield return m;
+                }
+            }
+        }
+    }
+
+    public (int minX, int minY, int maxX, int maxY) CalculateBounds()
+    {
+        var xRange = _grid.Keys.Select(k => k.Item1);
+        var yRange = _grid.Keys.Select(k => k.Item2);
+
+        return (xRange.Min(), yRange.Min(), xRange.Max(), yRange.Max());
+    }
+}
+
+public record GridMember<T>(int X, int Y)
+{
+    public GridMember(int X, int Y, T value) : this(X, Y) { _value = value; Exists = true; }
+
+    public bool Exists { get; init; } = false;
+
+    private readonly T? _value = default;
+
+    public T Value
+    {
+        get => Exists ? _value! : throw new NotSupportedException();
+    }
+
+    public static implicit operator GridMember<T>(T t) => new(0, 0, t);
+    public static implicit operator T(GridMember<T> g) => g.Value;
+}
